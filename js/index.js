e08e954e5c9eca1cee5436320ba91a6915766b0b
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-03-08 11:09:23 +0500

    Updayed File

--- js/index.js
+++ js/index.js
@@ -1,33 +1,37 @@
 const wrapper = document.querySelector(".wrapper");
 const form = wrapper.querySelector("form");
 const fileInp = form.querySelector("input");
-const infoText = form.querySelector('p');
-const copyBtn = document.querySelector('.copy');
+const infoText = form.querySelector("p");
+const copyBtn = wrapper.querySelector(".copy");
+const closeBtn = wrapper.querySelector(".close");
 
-function fetchRequest(formData, file){
-    infoText.innerText = 'Scanning QR Code...';
-    fetch('http://api.qrserver.com/v1/read-qr-code/', {
-        method: 'POST',
-        body: formData
-    }).then(res => res.json()).then(result => {
+function fetchRequest(formData, file) {
+    infoText.innerText = "Scanning QR Code...";
+    fetch("http://api.qrserver.com/v1/read-qr-code/", {
+        method: "POST",
+        body: formData,
+    })
+    .then((res) => res.json())
+    .then((result) => {
         result = result[0].symbol[0].data;
-        wrapper.querySelector('textarea').innerText = result;
-        form.querySelector('img').src = URL.createObjectURL(file);
-        infoText.innerText = 'Upload QR Code to Scan...'
-        wrapper.classList.add('active');
+        wrapper.querySelector("textarea").innerText = result;
+        form.querySelector("img").src = URL.createObjectURL(file);
+        infoText.innerText = "Upload QR Code to Scan...";
+        wrapper.classList.add("active");
     });
 }
 
-fileInp.addEventListener('change', e => {
+fileInp.addEventListener("change", (e) => {
     let file = e.target.files[0];
     let formData = new FormData();
-    formData.append('file', file);
-    fetchRequest(formData, file)
+    formData.append("file", file);
+    fetchRequest(formData, file);
 });
 
-copyBtn.addEventListener('click', () => {
-    let text = wrapper.querySelector('textarea').textContent;
+copyBtn.addEventListener("click", () => {
+    let text = wrapper.querySelector("textarea").textContent;
     navigator.clipboard.writeText(text);
 });
 
-form.addEventListener('click', () => fileInp.click())
+form.addEventListener("click", () => fileInp.click());
+closeBtn.addEventListener("click", () => wrapper.classList.remove("active"));
