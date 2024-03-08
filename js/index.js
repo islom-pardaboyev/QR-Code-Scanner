d17a3f55346fd8021cd301fc86fd338f3842c3d0
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-03-08 11:36:43 +0500

    Updated File

--- js/index.js
+++ js/index.js
@@ -6,31 +6,38 @@ const copyBtn = wrapper.querySelector(".copy");
 const closeBtn = wrapper.querySelector(".close");
 
 function fetchRequest(formData, file) {
-    infoText.innerText = "Scanning QR Code...";
-    fetch("http://api.qrserver.com/v1/read-qr-code/", {
-        method: "POST",
-        body: formData,
-    })
+  infoText.innerText = "Scanning QR Code...";
+  fetch("http://api.qrserver.com/v1/read-qr-code/", {
+    method: "POST",
+    body: formData,
+  })
     .then((res) => res.json())
     .then((result) => {
-        result = result[0].symbol[0].data;
-        wrapper.querySelector("textarea").innerText = result;
-        form.querySelector("img").src = URL.createObjectURL(file);
-        infoText.innerText = "Upload QR Code to Scan...";
-        wrapper.classList.add("active");
-    });
+      result = result[0].symbol[0].data;
+      infoText.innerText = result
+        ? "Upload QR Code to Scan..."
+        : "Could Scan QR Code";
+      if (!result) return;
+      wrapper.querySelector("textarea").innerText = result;
+      form.querySelector("img").src = URL.createObjectURL(file);
+
+      wrapper.classList.add("active");
+    }).catch(() => {
+        infoText.innerText = "Could Scan QR Code";
+    })
 }
 
 fileInp.addEventListener("change", (e) => {
-    let file = e.target.files[0];
-    let formData = new FormData();
-    formData.append("file", file);
-    fetchRequest(formData, file);
+  let file = e.target.files[0];
+  if (!file) return;
+  let formData = new FormData();
+  formData.append("file", file);
+  fetchRequest(formData, file);
 });
 
 copyBtn.addEventListener("click", () => {
-    let text = wrapper.querySelector("textarea").textContent;
-    navigator.clipboard.writeText(text);
+  let text = wrapper.querySelector("textarea").textContent;
+  navigator.clipboard.writeText(text);
 });
 
 form.addEventListener("click", () => fileInp.click());
